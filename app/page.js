dd09347856b6bb43ba0cbf5b766cdaf00125fe92
DEV SRIWARARAK <136062969+devsriwararak@users.noreply.github.com>
2023-12-24 01:48:40 +0700

    1

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>dev sriwararak รับเขียนโปรแกรมขอนแก่น</h1>
+      <p>dev sriwararak ทดสอบ next.js </p>
+    </div>
+  );
+}
