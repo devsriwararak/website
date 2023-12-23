8a5748d6eee2205618bc5fd3434e8310962d18ce
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2023-12-24 03:29:37 +0700

    24-12-2566-v-3

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-white shadow-lg px-20 py-5 sticky top-0 '>
+    <div className='bg-white shadow-lg px-20 py-5 w-full sticky top-0 '>
        <div className='flex justify-between'>
        <p className='font-semibold'>DEV SRIWARARAK</p>
        <ul className='flex flex-row gap-4'>
         <li>หน้าหลัก</li>
         <li>ฟอร์มจ้างทำงาน</li>
         <li>ผลงาน</li>
-        <li>เกี่ยวกับเรา</li>
-        <li>ติดต่อเรา</li>
-        <li>11</li>
+        {/* <li>เกี่ยวกับเรา</li>
+        <li>ติดต่อเรา</li> */}
+        
        </ul>
 
        </div>
--- app/components/Home.js
+++ app/components/Home.js
@@ -6,8 +6,11 @@ const Home = () => {
   return (
     <div>
       <div className="flex justify-between bg-purple-200">
-        <div className="px-20  py-10 flex flex-col justify-center gap-4">
-          <h1 className="text-5xl"><span className="font-semibold">DEV SRIWARARAK</span> รับเขียนโปรแกรม ทำเว็บไซต์ ขอนแก่น ครบวงจร </h1>
+        <div className=" px-10 md:px-20  py-10 flex flex-col justify-center gap-4">
+          <h1 className="text-5xl">
+            <span className="font-semibold">DEV SRIWARARAK</span>{" "}
+            รับเขียนโปรแกรม ทำเว็บไซต์ ขอนแก่น ครบวงจร{" "}
+          </h1>
           <p>
             dev sriwararak ทดสอบ next.js รับเขียนโปรแรกมขอนแก่น รับทำเว็บไซต์
             ขอนแก่น กำลังปรับปรุง{" "}
@@ -21,12 +24,9 @@ const Home = () => {
             </button>
           </div>
         </div>
-        <Image
-          src={banner}
-          loading="lazy"
-          width={600}
-          height={450}
-        />
+        <div className=" hidden md:block">
+          <Image src={banner} loading="lazy" width={600} height={450} />
+        </div>
       </div>
     </div>
   );
