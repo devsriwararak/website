0fec2fb05fc191388d8714f530765ded18bbdf2e
DEV SRIWARARAK <136062969+devsriwararak@users.noreply.github.com>
2023-12-24 02:25:06 +0700

    Add files via upload

---
+++ app/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-white shadow-lg px-10 py-5 sticky top-0 '>
+       <div className='flex justify-between'>
+       <p className='font-semibold'>DEV SRIWARARAK</p>
+       <ul className='flex flex-row gap-4'>
+        <li>11</li>
+        <li>11</li>
+        <li>11</li>
+        <li>11</li>
+        <li>11</li>
+       </ul>
+
+       </div>
+    </div>
+  )
+}
+
+export default Header
--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>dev sriwararak รับเขียนโปรแกรมขอนแก่น</h1>
       <p>dev sriwararak ทดสอบ next.js </p>
+
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    
     </div>
   );
 }
